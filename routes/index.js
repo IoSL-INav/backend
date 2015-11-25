f2ba8aed3c6f1562b43e01aebfc1a273cc806743
Andreas Hechenberger <poxy@greenstation.de>
2015-11-25 17:21:17 +0100

    edit index.js for new routs

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,9 @@ var config = require('./../config');
 var signup = require('./signup');
 var users = require('./users');
 var events = require('./events');
+var lists = require('./lists');
+var meetings = require('./meetings');
+var me = require('./me');
 
 var router = express.Router();
 
@@ -14,5 +17,8 @@ router.use(bodyParser.json());
 router.use('/', signup);
 router.use('/users', config.jwtMiddleware, users);
 router.use('/events', config.jwtMiddleware, events);
+router.use('/lists', config.jwtMiddleware, lists);
+router.use('/meetings', config.jwtMiddleware, meetings);
+router.use('/me', config.jwtMiddleware, me);
 
 module.exports = router;
