20e93ed82c023893737874ca224201ee55da74fa
Andreas Hechenberger <poxy@greenstation.de>
2016-01-30 15:54:35 +0100

    add more tests for users/me

--- test/test-backend.js
+++ test/test-backend.js
@@ -12,68 +12,158 @@ var supertest = require('supertest');
 var expect = require('chai').expect;
 var envFile = require('../.env.test');
 var backend = require('../app');
+var dummyGroup = {
+  _id: '',
+  name: "Some Groupname Here",
+  members: []
+};
 
+var dummyUser = {
+  userID: '',
+  userName: '',
+  userEmail: '',
+  userAutoPing: false,
+  userAutoLocate: false
+}
 
 /* Tests. */
 
 describe('Backend', function() {
 
-    it('should get the login page', function(done) {
-        supertest(backend)
-            .get('/users/me')
-            .set('Accept', 'application/json')
-            .expect(200)
-            .end(function(err, res) {
-                expect(res.body).to.have.property("userID");
-                expect(res.body).to.have.property("userName");
-                expect(res.body).to.have.property("userEmail");
-                done();
-            });
-    });
+  /* test user functions */
+  it('should get the login page', function(done) {
+    supertest(backend)
+      .get('/users/me')
+      .set('Accept', 'application/json')
+      .expect(200)
+      .end(function(err, res) {
+        expect(res.body).to.have.property("userID");
+        expect(res.body).to.have.property("userName");
+        expect(res.body).to.have.property("userEmail");
+        dummyUser.userID = res.body.userID;
+        dummyUser.userName = res.body.userName;
+        dummyUser.userEmail = res.body.userEmail;
+        done();
+      });
+  });
 
-    var gID='';
-    it('should create a group with the name "Some Groupname here"', function(done) {
-        supertest(backend)
-            .post('/users/me/groups')
-            .set('Accept', 'application/json')
-            .send({'groupName':'SomeGroupnameHere'})
-            .expect(200)
-            .end(function(err, res) {
-                expect(res.body).to.have.property("status");
-                expect(res.body).to.have.property("reason");
-                expect(res.body).to.have.property("groupID");
-                gID=res.body.groupID;
-                done();
-            });
-    });
+  it('should update user information', function(done) {
+    supertest(backend)
+      .put('/users/me')
+      .set('Accept', 'application/json')
+      .send({
+        "userName": dummyUser.userName,
+        "userAutoPing": dummyUser.userAutoPing,
+        "userAutoLocate": dummyUser.userAutoPing
+      })
+      .expect(200)
+      .end(function(err, res) {
+        expect(res.body).to.have.property("userID");
+        expect(res.body).to.have.property("userName");
+        expect(res.body).to.have.property("userEmail");
+        expect(res.body).to.have.property("userAutoPing");
+        expect(res.body).to.have.property("userAutoLocate");
+        if(res.body.userID != dummyUser.userID){
+          throw new Error("userID is not equal");
+        }
+        if(res.body.userName != dummyUser.userName){
+          throw new Error("userName is not equal");
+        }
+        if(res.body.userEmail != dummyUser.userEmail){
+          throw new Error("userEmail is not equal");
+        }
+        if(res.body.userAutoPing != dummyUser.userAutoPing){
+          throw new Error("userAutoPing is not equal");
+        }
+        if(res.body.userAutoLocate != dummyUser.userAutoLocate){
+          throw new Error("userAutoLocate is not equal");
+        }
+        done();
+      });
+  });
 
-    it('should get the created group back', function(done) {
-        supertest(backend)
-            .get('/users/me/groups/'+gID)
-            .set('Accept', 'application/json')
-            .expect(200)
-            .end(function(err, res) {
-              expect(res.body).to.have.property("_id");
-              expect(res.body).to.have.property("name");
-              expect(res.body).to.have.property("members");
-              if(res.body._id!=gID){
-                throw new Error("given groupID is not the same which is given back");
-              }
-              done();
-            });
-    });
+  it('should delete the user', function(done) {
+    supertest(backend)
+      .delete('/users/me/')
+      .set('Accept', 'application/json')
+      .expect(303)
+      .end(function(err, res) {
+        expect(res).to.have.property("status");
+        expect(res).to.have.property("redirect");
+        done();
+      });
+  });
 
-    it('should delte the created group', function(done) {
-        supertest(backend)
-            .delete('/users/me/groups/'+gID)
-            .set('Accept', 'application/json')
-            .expect(200)
-            .end(function(err, res) {
-              expect(res.body).to.have.property("status");
-              expect(res.body).to.have.property("reason");
-              done();
-            });
-    });
 
+  /* EOT - End of Test */
+
+  /* test group functions get,post,put,delete */
+  it('should create a group with the name "Some Groupname here"', function(done) {
+    supertest(backend)
+      .post('/users/me/groups')
+      .set('Accept', 'application/json')
+      .send({
+        'groupName': dummyGroup.name
+      })
+      .expect(200)
+      .end(function(err, res) {
+        expect(res.body).to.have.property("status");
+        expect(res.body).to.have.property("reason");
+        expect(res.body).to.have.property("groupID");
+        dummyGroup._id = res.body.groupID;
+        done();
+      });
+  });
+
+  it('should get the created group back', function(done) {
+    supertest(backend)
+      .get('/users/me/groups/' + dummyGroup._id)
+      .set('Accept', 'application/json')
+      .expect(200)
+      .end(function(err, res) {
+        expect(res.body).to.have.property("_id");
+        expect(res.body).to.have.property("name");
+        expect(res.body).to.have.property("members");
+        dummyGroup.members = res.body.members;
+        if (res.body._id != dummyGroup._id) {
+          throw new Error("given groupID is not the same which is given back");
+        }
+        done();
+      });
+  });
+
+  it('should update the groupname', function(done) {
+    dummyGroup.name = 'updateGroupName';
+    supertest(backend)
+      .put('/users/me/groups/' + dummyGroup._id)
+      .set('Accept', 'application/json')
+      .expect(200)
+      .send({
+        'newGroupName': dummyGroup.name
+      })
+      .end(function(err, res) {
+        expect(res.body).to.have.property("status");
+        expect(res.body).to.have.property("reason");
+        expect(res.body).to.have.property("group");
+        if (res.body.group._id != dummyGroup._id && res.body.group.name == dummyGroup.name && res.body.group.members.length == dummyGroup.members.length) {
+          throw new Error("given groupID,groupName or members length is not the same which is given back");
+        }
+        done();
+      });
+  });
+
+
+  it('should delete the created group', function(done) {
+    supertest(backend)
+      .delete('/users/me/groups/' + dummyGroup._id)
+      .set('Accept', 'application/json')
+      .expect(200)
+      .end(function(err, res) {
+        expect(res.body).to.have.property("status");
+        expect(res.body).to.have.property("reason");
+        done();
+      });
+  });
+  /* EOT - End of Test */
 
 });
